5a39c89626c18596291c98cd7663a3a25c2cada9
Fanny Miranda <Fanny@MacBook-Pro-de-Fanny.local>
2018-04-12 19:16:52 +0200

    primer commit del proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+    /*$(window).scroll(function() {
+        if ($("#menu").scrollTop() >=400){
+        $("#menu").addClass("bg-inverse");
+    } else {
+        $("#menu").removeClass("bg-inverse");
+    }
+    });*/
+
+$("body").on("scroll",function(){
+    console.log("hola");
+});
